93adaa5518c513b3fd5bbf38ca1eff01ade9be9d
Priyaaaaanu <priyar2962001@gmail.com>
2024-12-14 19:42:39 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/ecommerce_scaler/EcommerceScalerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_scaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceScalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceScalerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommerce_scaler/EcommerceScalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_scaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceScalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
